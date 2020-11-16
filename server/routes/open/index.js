445f917cd284b949d02c3847fe496848e6abd016
danchio <danielchiong3@gmail.com>
2020-11-15 20:24:20 -0500

    added login funtionality

--- server/routes/open/index.js
+++ server/routes/open/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router(),
-  { createUser } = require('../../controllers/users');
+  { createUser, loginUser } = require('../../controllers/users');
 
 // JUST FOR DEMO PURPOSES, PUT YOUR ACTUAL API CODE HERE
 router.post('/', createUser);
+router.post('/login', loginUser);
 // END DEMO
 
 module.exports = router;
