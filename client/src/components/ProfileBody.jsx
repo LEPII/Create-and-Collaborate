efbd4a8d76bf16e8615484a117efad1eba98b8f5
danchio <danielchiong3@gmail.com>
2020-11-20 22:00:59 -0500

    profile is coming along!

--- client/src/components/ProfileBody.jsx
+++ client/src/components/ProfileBody.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import VideoUploader from '../components/VideoUploader';
+import VideoUploader from '../helper/VideoUploader';
 
 const ProfileBody = () => {
   return (
-    <div className="profaction">
-      <VideoUploader />
-      <button type="button" class="btn btn-secondary">
-        <img src="https://via.placeholder.com/100px" alt="user" />
-      </button>
-      <button type="button" class="btn btn-secondary">
-        <img src="https://via.placeholder.com/100px" alt="user" />
-      </button>
-      <button type="button" class="btn btn-secondary">
-        <img src="https://via.placeholder.com/100px" alt="user" />
-      </button>
+    <div>
+      <div className="profaction">
+        <VideoUploader />
+        <button type="button" class="btn btn-secondary">
+          <img src="https://via.placeholder.com/100px" alt="user" />
+        </button>
+        <button type="button" class="btn btn-secondary">
+          <img src="https://via.placeholder.com/100px" alt="user" />
+        </button>
+        <button type="button" class="btn btn-secondary">
+          <img src="https://via.placeholder.com/100px" alt="user" />
+        </button>
+      </div>
+      <div className="profbody">
+        <button type="button" class="btn btn-secondary">
+          <img src="https://via.placeholder.com/100px" alt="user" />
+        </button>
+        <button type="button" class="btn btn-secondary">
+          <img src="https://via.placeholder.com/100px" alt="user" />
+        </button>
+        <button type="button" class="btn btn-secondary">
+          <img src="https://via.placeholder.com/100px" alt="user" />
+        </button>
+      </div>
     </div>
   );
 };
