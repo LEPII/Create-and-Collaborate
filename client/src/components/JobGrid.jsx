48868b331e814b87ecd8f4a9d9661adb8d50303e
danchio <danielchiong3@gmail.com>
2020-11-22 02:33:18 -0500

    jobs are being displayed properly! working on searcg functionality now.

--- client/src/components/JobGrid.jsx
+++ client/src/components/JobGrid.jsx
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import '../jobs.css';
 import axios from 'axios';
-import { useContext } from '../context/AppContext';
+import { Card } from 'react-bootstrap';
 import JobSearch from './JobSearch';
 
 const JobGrid = () => {
-  // const {
-  //   search,
-  //   loading
-  // } = useContext(AppContext);
   const [jobs, setJobs] = useState(null);
+  const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
     axios
@@ -23,10 +20,19 @@ const JobGrid = () => {
       });
   }, [setJobs]);
 
+  const handleSearch = (searchTerm) => {
+    setSearchValue(searchTerm);
+  };
+  const filteredJobs =
+    jobs && jobs.filter((jobs) => jobs.title.includes(searchValue));
+
   return (
-    <>
-      <JobSearch />
-      <table class="table table-bordered">
+    <div className="jobContainer">
+      <Card>
+        <Card.Img variant="top" src="https://via.placeholder.com/400x100" />
+      </Card>
+      <JobSearch handleSearch={handleSearch} />
+      <table class="table">
         {jobs &&
           jobs.map((job) => {
             return (
@@ -48,63 +54,8 @@ const JobGrid = () => {
             );
           })}
       </table>
-    </>
+    </div>
   );
-
-  // return (
-
-  //   <div className="gridBG">
-  //     <div className="container gridInnards">
-  //       <table class="table table-bordered">
-  //         <thead>
-  //           <tr>
-  //             <th scope="col"></th>
-  //             <th scope="col">Position</th>
-  //             <th scope="col">Description</th>
-  //           </tr>
-  //         </thead>
-  //         <tbody>
-  //           <tr>
-  //             <th scope="row">
-  //               <img src="https://via.placeholder.com/100x100" />
-  //             </th>
-  //             <td>Actor</td>
-  //             <td colspan="2">
-  //               Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-  //               quos reiciendis dolores rerum praesentium doloremque laboriosam
-  //               id sequi quibusdam nihil tempora nemo voluptatum vel, in error
-  //               amet ratione velit. Tenetur?
-  //             </td>
-  //           </tr>
-  //           <tr>
-  //             <th scope="row">
-  //               <img src="https://via.placeholder.com/100x100" />
-  //             </th>
-  //             <td>Guitarist</td>
-  //             <td colspan="2">
-  //               Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-  //               quos reiciendis dolores rerum praesentium doloremque laboriosam
-  //               id sequi quibusdam nihil tempora nemo voluptatum vel, in error
-  //               amet ratione velit. Tenetur?
-  //             </td>
-  //           </tr>
-  //           <tr>
-  //             <th scope="row">
-  //               <img src="https://via.placeholder.com/100x100" />
-  //             </th>
-  //             <td>Emcee</td>
-  //             <td colspan="2">
-  //               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-  //               Voluptatibus fugiat ea, aperiam, totam doloribus necessitatibus
-  //               sint ex possimus facere dolorem dolor et quidem alias commodi
-  //               iste tempore obcaecati fuga quia!
-  //             </td>
-  //           </tr>
-  //         </tbody>
-  //       </table>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default JobGrid;
