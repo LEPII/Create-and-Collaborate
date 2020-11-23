c28a62dede579f817e65f261bba9264f305ec9db
danchio <danielchiong3@gmail.com>
2020-11-22 22:14:06 -0500

    added functionality to navbar to have a logout button, just need to add history method to push to welcome page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavDropdown, NavbarBrand } from 'react-bootstrap';
-import EventSearch from './EventSearch';
+import React, { useContext } from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { AppContext } from '../context/AppContext';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 const NavBar = () => {
+  const { setCurrentUser, user } = useContext(AppContext);
+  const history = useHistory();
+  const logout = async () => {
+    try {
+      const response = await axios({
+        method: 'POST',
+        url: '/users/logout',
+        withCredentials: true
+      });
+      sessionStorage.removeItem('user');
+      setCurrentUser(null).then(() => history.push('/welcome'));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand href="/">C & C</Navbar.Brand>
@@ -12,20 +29,16 @@ const NavBar = () => {
         <Nav className="mr-auto">
           <Nav.Link href="jobs">Gigs</Nav.Link>
           <Nav.Link href="events">Events</Nav.Link>
-          <NavDropdown title="" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
         </Nav>
         <Nav>
-          <Nav.Link href="welcome">User</Nav.Link>
+          <NavDropdown
+            title=""
+            className="dropleft"
+            id="collasible-nav-dropdown"
+          >
+            <NavDropdown.Item onClick={logout}>Log Out</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="/">User</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
