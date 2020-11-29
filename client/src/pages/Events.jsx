589a4327e4a6cdc549f1a9645631a69b58228b42
danchio <danielchiong3@gmail.com>
2020-11-29 13:19:29 -0500

    added a job form page for user to create jobs

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 import EventsHead from '../components/EventsHead';
 import EventCalendar from '../helper/EventCalendar';
-import EventSearch from '../components/EventSearch';
 
 function Events() {
   return (
@@ -11,7 +9,6 @@ function Events() {
       <Navbar />
       <EventsHead />
       <EventCalendar />
-      {/* <Footer /> */}
     </div>
   );
 }
